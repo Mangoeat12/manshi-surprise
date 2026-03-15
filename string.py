3406d91d455f986e2b49b5b65bd8ce144002b3cf
Mangoeat12 <140334078+Mangoeat12@users.noreply.github.com>
2026-03-15 11:26:20 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,25 @@
+data = 10 
+data1 =" 10hi"
+
+# print(type(data), type(data1))
+
+# print(data1[10])
+
+#for ermoving extra spaces from a string. then use .strip() which remove the extra spaces from a string 
+#for removing the right spaces only use rstrip() 
+#for removing the left spaces only use lstrip()
+
+# print(data1.strip())
+
+
+
+# -------------
+
+#replacing the string with new one so use this 
+#.replace(replace string, new string)
+
+
+replace = input("Enter your name: ")
+
+if replace.upper() == "AYUSH":
+    print("Welcome Mango")
